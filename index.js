09ce3cf7250cb7c4b2a41959261dc606ca60af49
Sho-ryuu <74526355+Sho-ryuu@users.noreply.github.com>
2020-11-16 14:10:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,10 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
-const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const BotName = 'Dika BOT☄'; 
+const instagram = 'https://instagram.com/endkm.tm'; 
+const telegram = 'https://t.me/dikaa28'; 
+const kapanbotaktif = 'KETIKA OWNER AKTIF'; 
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
